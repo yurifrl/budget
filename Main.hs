2c656a1b6a8d9c77745ac7809861a051ee9eefee
yuri <yurifl03@gmail.com>
2020-01-13 11:48:08 -0300

    wip

--- Main.hs
+++ Main.hs
@@ -1,5 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
+
 import Budget
 
+import System.Envy (FromEnv, decodeEnv)
+import GHC.Generics (Generic)
+
+data NBConfig = NBConfig {
+  nbLogin    :: String
+, nbPassword :: String
+} deriving (Generic, Show)
+
+instance FromEnv NBConfig
+
 main :: IO ()
-main = putStrLn $ show $ Budget.double 4
+main = do
+  nbConfig <- decodeEnv :: IO (Either String NBConfig)
+  case nbConfig of
+    Left l -> fail $ show l
+    Right r -> print r
--- README.md
+++ README.md
@@ -11,3 +11,6 @@
 - https://github.com/ynab/ynab-sdk-js
 - https://github.com/andreroggeri/pynubank
 - https://taylor.fausak.me/2014/03/04/haskeleton-a-haskell-project-skeleton/
+- https://gist.github.com/tfausak/cdcc2e33c9c418278262650eb96b7b05#file-main-hs
+- https://hoogle.haskell.org/
+- https://github.com/tfausak/rocket-league-api/blob/master/rocket-league-api.hs
